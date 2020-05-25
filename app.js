ee040d30f280f1c9076ceda392fb8710dde54311
John Doeirma <irmaaa96@hotmail.com>
2020-05-25 23:13:11 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const logger = require('morgan');
+ 
+const indexRouter = require('./routes/index');
+ 
+var app = express();
+ 
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+ 
+app.use('/', indexRouter);
+ 
+module.exports = app;
